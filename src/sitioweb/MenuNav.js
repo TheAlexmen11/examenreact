47dc851d5884688fc26621c2a38521af853ea18a
Alex Michael <alexdcancho@gmail.com>
2021-08-04 17:26:03 -0500

    mejora en nav

--- src/sitioweb/MenuNav.js
+++ src/sitioweb/MenuNav.js
@@ -5,27 +5,27 @@ const MenuNav = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light p-3" style={{backgroundColor: 'rgb(255 255 244)'}}>
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">GAMING</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  <a className="nav-link active" aria-current="page" href="https://dragon-technology-eirl.netlify.app/">Home</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Link</a>
+                  <a className="nav-link" href="http://thealexmen11.epizy.com/">Pagina 01</a>
                 </li>
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
+                    SubMenu
                   </a>
                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Action</a></li>
-                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="http://thealexmen11.epizy.com/informacion/">Informacion</a></li>
+                    <li><a className="dropdown-item" href="http://thealexmen11.epizy.com/ubicacion/">Galeria Laptops</a></li>
                     <li><hr className="dropdown-divider"/></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li><a className="dropdown-item" href="#">stock(proximamente)</a></li>
                   </ul>
                 </li>
                 <li className="nav-item">
